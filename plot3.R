d485785fab877dd3c0841e89e2d07e4d176f918f
oerciyes <oerciyes@hotmail.com>
2015-06-05 11:56:19 -0500

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,20 @@
+#HPC-Plot 3
+HPC<-read.delim("household_power_consumption.txt",header=T,sep=";",na.strings="?")
+summary(HPC)
+str(HPC)
+HPC1 <- subset(HPC, Date=="1/2/2007"|Date =="2/2/2007")
+summary(HPC1)
+str(HPC1)
+
+HPC1$Sub_metering_1 <- as.numeric(as.character(HPC1$Sub_metering_1))
+HPC1$Sub_metering_2 <- as.numeric(as.character(HPC1$Sub_metering_2))
+HPC1$Sub_metering_3 <- as.numeric(as.character(HPC1$Sub_metering_3))
+HPC1$day <- as.POSIXct(strptime(paste(HPC1$Date,HPC1$Time), " %d/%m/%Y %H:%M:%S", tz="EST"))
+png(filename="Plot3.png",width=480,height=480)
+plot(HPC1$day,HPC1$Sub_metering_1,xlab="", ylab="Energy sub metering", type= "n")
+lines(HPC1$day, HPC1$Sub_metering_1, type="l", col="black")
+lines(HPC1$day, HPC1$Sub_metering_2, type="l", col="red")
+lines(HPC1$day, HPC1$Sub_metering_3, type="l", col="blue")
+
+legend("topright",lty=1, col=c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty="c")
+dev.off()
